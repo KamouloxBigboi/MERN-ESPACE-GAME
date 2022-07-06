20e60a9010749fee25fae0f9ee093b546e9cdcc0
KamouloxBigboi <kamal.guidadou@gmail.com>
2022-07-06 17:13:52 +0200

    tested images on front page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The Extreme Pirate Island Espace Game
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click here to start your journey
         </a>
       </header>
     </div>
